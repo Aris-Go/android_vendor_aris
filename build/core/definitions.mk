0bef0ca7fe71537075bcb74999cd1b365c385a44
Steve Kondik <steve@cyngn.com>
2022-07-30 19:51:43 +0800

    build: core: Aris-Go-specific build macros
    * vendor/*/build/core/definitions.mk is automatically sourced by the
       build system to load custom macros. Start using this for some
       of our own.
     * Adds a "uniq" macro to remove duplicates from a list without changing
       it's order.

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -1,4 +1,11 @@
-# Include QCOM macros
+#
+# Aris-Go-specific macros
+#
+define uniq
+$(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
+endef
+
+# Include board/platform macros
 include vendor/aris/build/core/utils.mk
 
 # Include vendor platform definitions
