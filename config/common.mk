7b1eb2c77c217bde32004afd72b772c14861a157
XayahSuSuSu <zds1249475336@gmail.com>
2022-07-30 14:08:42 +0800

    build: Add modular build hooks
    * Ref: https://github.com/exTHmUI/android_vendor_exthm

---
+++ config/common.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2024 The Aris-Go Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Aris version
+-include vendor/aris/config/version.mk
---
+++ config/version.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Aris-Go Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ARIS_VERSION := 14.0
+
+ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.build.version.custom=$(ARIS_VERSION)
