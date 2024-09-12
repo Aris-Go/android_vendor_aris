CSC-UI/android_vendor_csc

build: core: Clean up makefile inclusions
* Include QCOM rules directly from the build/core/config.mk hook.

Change-Id: Iad2f808f3e3eefbd2be2ca5facb907ff31babbb7

build/core/config.mk
@@ -1,0 +1,17 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#           (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Rules for QCOM targets
+include $(TOPDIR)vendor/acme/build/core/qcom_target.mk

Aris-Go/android_vendor_aris

build/core/config.mk
@@ -1,0 +1,18 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#           (C) 2017-2018 The LineageOS Project
+#           (C) 2024 The Aris-Go Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Rules for QCOM targets
+include $(TOPDIR)vendor/aris/build/core/qcom_target.mk