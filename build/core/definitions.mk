exthmui-aosp/android_vendor_exthm

build: Reorganize inclusion of QCOM macros * There's actually not a whole lot QC-specific about these macros, make them available at definition.mk inclusion time rather than relying on BoardConfig to include them. * Don't create a dependency on QCOM macros from a makefile that is included very early in the build process.

build/core/definitions.mk
@@ -1,0 +1,2 @@
+# Include QCOM macros
+include vendor/exthm/build/core/qcom_utils.mk

Aris-Go/android_vendor_aris

build/core/definitions.mk
@@ -1,0 +1,2 @@
+# Include QCOM macros
+include vendor/aris/build/core/qcom_utils.mk