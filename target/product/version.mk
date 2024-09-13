6c3ef97527aa62451b0743d2951f8e9c836080b7
kmiit <61952405+kmiit@users.noreply.github.com>
2024-09-14 00:54:28 +0800

    version: Update to Android 15

--- target/product/version.mk
+++ target/product/version.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-ARIS_VERSION_NUM := 14.0
-ARIS_BRANCH := Ui
+ARIS_VERSION_NUM := 15.0
+ARIS_BRANCH := Vivi
 ARIS_BUILD_TYPE := WEEKLY
 ARIS_DATE := $(shell date -u +%Y%m%d)
 ARIS_DEVICE := $(shell echo "$(TARGET_PRODUCT)" | cut -d '_' -f 2,3)
