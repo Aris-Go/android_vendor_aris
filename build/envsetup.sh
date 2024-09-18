LineageOS/android_vendor_lineage

build: Import check_product from lineage-21.0 build/make
Change-Id: I6cb2856bbc15bc25fc3ad6a4c1a6aa6aa56057f1

build/envsetup.sh
@@ -53,6 +53,30 @@ function mk_timer()
     return $ret
 }
 
+# check to see if the supplied product is one we can build
+function check_product()
+{
+    local T=$(gettop)
+    if [ ! "$T" ]; then
+        echo "Couldn't locate the top of the tree. Try setting TOP." >&2
+        return
+    fi
+    if (echo -n $1 | grep -q -e "^lineage_") ; then
+        LINEAGE_BUILD=$(echo -n $1 | sed -e 's/^lineage_//g')
+    else
+        LINEAGE_BUILD=
+    fi
+    export LINEAGE_BUILD
+
+        TARGET_PRODUCT=$1 \
+        TARGET_RELEASE=$2 \
+        TARGET_BUILD_VARIANT= \
+        TARGET_BUILD_TYPE= \
+        TARGET_BUILD_APPS= \
+        get_build_var TARGET_DEVICE > /dev/null
+    # hide successful answers, but allow the errors to show
+}
+
 function brunch()
 {
     breakfast $*

Aris-Go/android_vendor_aris

build/envsetup.sh
@@ -1,3 +1,27 @@
+# check to see if the supplied product is one we can build
+function check_product()
+{
+    local T=$(gettop)
+    if [ ! "$T" ]; then
+        echo "Couldn't locate the top of the tree. Try setting TOP." >&2
+        return
+    fi
+    if (echo -n $1 | grep -q -e "^aris_") ; then
+        ARIS_BUILD=$(echo -n $1 | sed -e 's/^aris_//g')
+    else
+        ARIS_BUILD=
+    fi
+    export ARIS_BUILD
+
+        TARGET_PRODUCT=$1 \
+        TARGET_RELEASE=$2 \
+        TARGET_BUILD_VARIANT= \
+        TARGET_BUILD_TYPE= \
+        TARGET_BUILD_APPS= \
+        get_build_var TARGET_DEVICE > /dev/null
+    # hide successful answers, but allow the errors to show
+}
+
 function brunch()
 {
     breakfast $*