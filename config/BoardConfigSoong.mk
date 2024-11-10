ashkorehennessy/android_vendor_exthm

soong: Add `aapt_version_code` default
This appends `--version_code=$(date -u +%Y%m%d)` to aapt flags, which is
useful for flushing some caches upon system updates.

config/BoardConfigSoong.mk
@@ -28,6 +28,7 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += exthmGlobalVars
 SOONG_CONFIG_exthmGlobalVars += \
+    aapt_version_code \
     additional_gralloc_10_usage_bits \
     gralloc_handle_has_custom_content_md_reserved_size \
     gralloc_handle_has_reserved_size \
@@ -79,6 +80,7 @@ TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
+SOONG_CONFIG_exthmGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
 SOONG_CONFIG_exthmGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_exthmGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_exthmGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))

Aris-Go/android_vendor_aris

config/BoardConfigSoong.mk
@@ -28,6 +28,7 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += arisGlobalVars
 SOONG_CONFIG_arisGlobalVars += \
+    aapt_version_code \
     additional_gralloc_10_usage_bits \
     gralloc_handle_has_custom_content_md_reserved_size \
     gralloc_handle_has_reserved_size \
@@ -49,6 +50,7 @@ SOONG_CONFIG_arisQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_arisGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
 SOONG_CONFIG_arisGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
 SOONG_CONFIG_arisGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_arisGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)