exthmui-aosp/android_vendor_exthm

soong: Add TARGET_USES_EGL_DISPLAY_ARRAY conditional This soong variable is used to conditionally revert a commit [1] which causes random camera crashes on tama devices
[1] LineageOS/android_frameworks_native@a9550f3

config/BoardConfigSoong.mk
@@ -34,7 +34,8 @@ SOONG_CONFIG_exthmGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_process_sdk_version_override \
-    target_surfaceflinger_udfps_lib
+    target_surfaceflinger_udfps_lib \
+    uses_egl_display_array
 
 SOONG_CONFIG_NAMESPACES += exthmQcomVars
 SOONG_CONFIG_exthmQcomVars += \
@@ -50,6 +51,7 @@ endif
 # Soong bool variables
 SOONG_CONFIG_exthmGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
 SOONG_CONFIG_exthmGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
+SOONG_CONFIG_exthmGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_exthmQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_exthmQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 

Aris-Go/android_vendor_aris

config/BoardConfigSoong.mk
@@ -33,7 +33,8 @@ SOONG_CONFIG_arisGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_process_sdk_version_override \
-    target_surfaceflinger_udfps_lib
+    target_surfaceflinger_udfps_lib \
+    uses_egl_display_array
 
 SOONG_CONFIG_NAMESPACES += arisQcomVars
 SOONG_CONFIG_arisQcomVars += \
@@ -50,6 +51,7 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_arisGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
+SOONG_CONFIG_arisGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_arisQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_arisQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
 SOONG_CONFIG_arisQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_PERF)